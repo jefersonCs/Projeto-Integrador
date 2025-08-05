bc236e3ec5cdd500382a9f6d1abff134ec87ec56
JULIANE CARVALHO <gemesulva1964@gmail.com>
2025-08-05 10:03:56 -0300

    nada

--- script/protect-page.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const usuario = JSON.parse(localStorage.getItem('usuarioDados'));
-  
-    if (!usuario || !usuario.email) {
-      alert('Você precisa estar logado para acessar esta página.');
-      window.location.href = 'index.html';
-    }
-  });
-  
